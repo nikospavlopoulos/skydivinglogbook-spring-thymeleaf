4eeca436f05417da12c7b10ba146bac6486de55b
Nikos Pavlopoulos <npavlopoulos@gmail.com>
2025-01-25 16:42:13 +0200

    Initial Commit

---
+++ src/main/java/com/nikospavlopoulos/skydivinglogbook/SkydivingLogbookApplication.java
@@ -0,0 +1,13 @@
+package com.nikospavlopoulos.skydivinglogbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkydivingLogbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkydivingLogbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nikospavlopoulos/skydivinglogbook/SkydivingLogbookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikospavlopoulos.skydivinglogbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkydivingLogbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
